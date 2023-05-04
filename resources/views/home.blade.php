f7e9c14d72ac831faeb59a40f42fa5538074e443
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-04 18:44:15 +0300

    view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,5 +11,28 @@
 
 <body>
 
+    <div class="container py-3">
+
+        <div class="row">
+
+        @foreach ($movies as $movie)
+
+            <div class="col-3 mb-3">
+                <div class="card h-100">
+                    <div class="card-body border border-5 border-warning bg-success">
+                        <h1 class="card-title">{{ $movie->title }}</h1>
+                        <p class="card-text">Original title: {{ $movie->original_title }}</p>
+                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                        <p class="card-text">Date: {{ $movie->date }}</p>
+                        <p class="card-text">Vote: {{ $movie->vote }}</p>
+                    </div>
+                </div>
+            </div>
+
+        @endforeach
+        
+        </div>
+
+    </div>
     
 </body>
