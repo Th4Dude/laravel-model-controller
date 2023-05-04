f94d71b3f62f14daefb0586fdb95cdb4120be4c9
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-04 18:02:11 +0300

    pageController + route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-        <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
-        </div>
+<html lang="en">
 
-    </body>
-</html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    
+</body>
